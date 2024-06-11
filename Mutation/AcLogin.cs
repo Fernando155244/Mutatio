23ed23f1f58ebc41269c1e2363f5c11e26bd240a
Fercho <160249039+Fernando155244@users.noreply.github.com>
2024-06-11 16:57:00 -0600

    Renombrado todo y conectado conectada cada pantalla con su actividad

---
+++ Mutation/AcLogin.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mutation
+{
+    [Activity(Label = "AcLogin")]
+    public class AcLogin : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Login);
+        }
+    }
+}
